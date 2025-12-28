cdc82d2469b4e21b0a96c13ecd3b24e38843aedf
Abhay Kumar <157803348+abhaykumar30@users.noreply.github.com>
2025-12-28 20:38:42 +0530

    Add files via upload
    Hello java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        int num = 5;
+        int num1 = 7;
+        int sum = num + num1;
+
+        System.out.println("Hello Abhay");
+        System.out.println("The Sum of values is : " + sum);
+
+    }
+}

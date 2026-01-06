ca73dbb413b2ca0d0c5c2c47e571c8149c06b8c4
Abhay Kumar <157803348+abhaykumar30@users.noreply.github.com>
2026-01-06 21:09:45 +0530

    Loop
    Loop

---
+++ Loop.java
@@ -0,0 +1,28 @@
+public class Loop {
+    public static void main(String a[]){
+        
+        //While Loop
+        int i = 1;
+        while(i<=5){
+            System.out.println(i + " Hi");
+            i++;
+        }
+        System.out.println(i + " Bye While");
+
+
+        // For Loop;
+        for(int k = 1; k <= 10; k++){
+            System.out.println(k*2);
+        }
+
+
+        //Do-while
+        System.out.println("Do-While Loop");
+        int j = 1;
+        do{
+            System.out.println(j*3);
+            j++;
+        }while(j < 10);
+
+    }
+}

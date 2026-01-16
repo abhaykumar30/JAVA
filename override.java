728e01ae299ae294fbf74257bfe317aef343157e
Abhay Kumar <157803348+abhaykumar30@users.noreply.github.com>
2026-01-16 20:36:51 +0530

    Add files via upload

---
+++ override.java
@@ -0,0 +1,33 @@
+
+class A{
+    A(){
+        super();
+        System.out.println("Example of Super keyword");
+    }
+    public void show(){
+        System.out.println("In A show");
+    }
+
+    public void con(){
+        System.out.println("In A can");
+    }
+}
+
+class B extends A{
+    //this show override A's show method.
+    B(){
+        super();
+        System.out.println("Example of Super keyword");
+    }
+    public void show(){
+        System.out.println("In B show");
+    }
+}
+
+public class override {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show();
+        obj.con();
+    }
+}

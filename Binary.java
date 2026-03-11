2afec3d927f37083292af2d904d589abf0f9ab70
Abhay Kumar <157803348+abhaykumar30@users.noreply.github.com>
2026-03-11 17:15:33 +0530

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int binarysearch(int[] arr, int k) {
+        // Code Here
+        int s = 0;
+        int e = arr.length - 1;
+        int result = -1;
+        
+        while(s <= e){
+            int mid =  s + (e - s) / 2;
+            
+            if(k == arr[mid]){
+                result = mid;
+                e = mid - 1;
+            }
+            else if(k > arr[mid]){
+                s = mid +1;
+            }
+            else{
+                e = mid -1;
+            }
+        }
+        
+        return result;
+    }
+}

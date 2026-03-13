a99bfa4195cf7adf5c6cad752b3526c2193f1826
Abhay Kumar <157803348+abhaykumar30@users.noreply.github.com>
2026-03-13 21:47:55 +0530

    13/03

--- DSA/Binary.java
+++ DSA/Binary.java
@@ -1,27 +1,39 @@
-class meth {
-    int search(int arr[], int s, int e, int key) {
-        while (s <= e) {
+public class Binary {
+    public static int search(int arr[], int s, int e, int key) {
+
+        // Noraml binary Search
+        // while (s <= e) {
+        // int mid = (s + e) / 2;
+        // if (arr[mid] == key) {
+        // return mid;
+        // } else if (arr[mid] < key) {
+        // s = mid + 1;
+        // } else {
+        // e = mid - 1;
+        // }
+        // }
+        // return -1;
+
+        // Recursion
+        int result = -1;
+        if (s <= e) {
             int mid = (s + e) / 2;
             if (arr[mid] == key) {
-                return mid;
+                result = mid;
             } else if (arr[mid] < key) {
-                s = mid + 1;
+                result = search(arr, mid + 1, e, key);
             } else {
-                e = mid - 1;
+                result = search(arr, s, mid - 1, key);
             }
         }
-        return -1;
+        return result;
     }
 
-}
-
-public class Binary {
     public static void main(String[] args) {
-        meth obj = new meth();
 
         int[] arr = { 10, 20, 30, 40, 50 };
 
-        int ans = obj.search(arr, 0, arr.length - 1, 50);
+        int ans = search(arr, 0, arr.length - 1, 50);
 
         if (ans != -1) {
             System.out.println("Key found on : " + ans + " index;");
